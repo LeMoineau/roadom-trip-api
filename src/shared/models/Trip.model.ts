5fd8c83242f1c2d89877cf2ba5c80bd477cbe785
Pierrot <faberbastian@gmail.com>
2026-02-01 22:46:19 +0100

    begin adding hints

--- src/shared/models/Trip.model.ts
+++ src/shared/models/Trip.model.ts
@@ -4,25 +4,35 @@ import { UUID } from "../types/primitives/Identifier";
 import { GeoPoint } from "./GeoPoint.model";
 import { v4 as uuidv4 } from "uuid";
 
-let TRIP_ID_INCREMENTOR = 1;
-
 export class Trip {
   id: UUID;
   startingPos: GeoPoint;
   endingPos: GeoPoint;
+  createdAt: Date;
 
   constructor({
     startingPos,
     endingPos,
-    id = `${TRIP_ID_INCREMENTOR++}`,
+    id = uuidv4(),
+    createdAt = new Date(),
   }: {
     startingPos: GeoPointDto;
     endingPos: GeoPointDto;
     id?: UUID;
+    createdAt?: Date | string;
   }) {
     this.id = id;
     this.startingPos = new GeoPoint(startingPos);
     this.endingPos = new GeoPoint(endingPos);
+    if (typeof createdAt === "string") {
+      try {
+        this.createdAt = new Date(createdAt);
+      } catch (err) {
+        this.createdAt = new Date();
+      }
+    } else {
+      this.createdAt = createdAt;
+    }
   }
 
   toDto(): TripDto {
@@ -30,6 +40,7 @@ export class Trip {
       id: this.id,
       startingPos: this.startingPos.toDto(),
       endingPos: this.endingPos.toDto(),
+      createdAt: this.createdAt.toString(),
     };
   }
 }
