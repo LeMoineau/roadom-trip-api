e01f4c6e5e4f36cef2e96417fe1160d4b08f392e
Pierre Faber <faberbastian@gmail.com>
2026-02-09 20:58:23 +0100

    adding blasons + flags list

---
+++ src/constants/blasons.ts
@@ -0,0 +1,602 @@
+export const departementsBlasons = [
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/01-Ain.jpg",
+    departementCode: "01",
+    departementLibelle: "Ain",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/02-Aisne.jpg",
+    departementCode: "02",
+    departementLibelle: "Aisne",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/03-Allier.jpg",
+    departementCode: "03",
+    departementLibelle: "Allier",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/04-Alpes_de_Haute_Provence.jpg",
+    departementCode: "04",
+    departementLibelle: "Alpes de Haute Provence",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/05-Hautes_Alpes.jpg",
+    departementCode: "05",
+    departementLibelle: "Hautes Alpes",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/06-Alpes_Maritimes.jpg",
+    departementCode: "06",
+    departementLibelle: "Alpes Maritimes",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/07-Ardeche.jpg",
+    departementCode: "07",
+    departementLibelle: "Ardèche",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/08-Ardennes.jpg",
+    departementCode: "08",
+    departementLibelle: "Ardennes",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/09-Ariege.jpg",
+    departementCode: "09",
+    departementLibelle: "Ariège",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/10-Aube.jpg",
+    departementCode: "10",
+    departementLibelle: "Aube",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/11-Aude.jpg",
+    departementCode: "11",
+    departementLibelle: "Aude",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/12-Aveyron.jpg",
+    departementCode: "12",
+    departementLibelle: "Aveyron",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/13-Bouches_du_Rhone.jpg",
+    departementCode: "13",
+    departementLibelle: "Bouches du Rhône",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/14-Calvados.jpg",
+    departementCode: "14",
+    departementLibelle: "Calvados",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/15-Cantal.jpg",
+    departementCode: "15",
+    departementLibelle: "Cantal",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/16-Charente.jpg",
+    departementCode: "16",
+    departementLibelle: "Charente",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/17-Charente_Maritime.jpg",
+    departementCode: "17",
+    departementLibelle: "Charente Maritime",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/18-Cher.jpg",
+    departementCode: "18",
+    departementLibelle: "Cher",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/19-Correze.jpg",
+    departementCode: "19",
+    departementLibelle: "Corrèze",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/20-Corse.jpg",
+    departementCode: "20",
+    departementLibelle: "Corse du Sud et Haute Corse",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/21-Cote_d_Or.jpg",
+    departementCode: "21",
+    departementLibelle: "Côte d'Or",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/22-Cotes_d_Armor.jpg",
+    departementCode: "22",
+    departementLibelle: "Côtes d'Armor",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/23-Creuse.jpg",
+    departementCode: "23",
+    departementLibelle: "Creuse",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/24-Dordogne.jpg",
+    departementCode: "24",
+    departementLibelle: "Dordogne",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/25-Doubs.jpg",
+    departementCode: "25",
+    departementLibelle: "Doubs",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/26-Drome.jpg",
+    departementCode: "26",
+    departementLibelle: "Drôme",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/27-Eure.jpg",
+    departementCode: "27",
+    departementLibelle: "Eure",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/28-Eure_et_Loir.jpg",
+    departementCode: "28",
+    departementLibelle: "Eure et Loir",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/29-Finistere.jpg",
+    departementCode: "29",
+    departementLibelle: "Finistère",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/30-Gard.jpg",
+    departementCode: "30",
+    departementLibelle: "Gard",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/31-Haute_Garonne.jpg",
+    departementCode: "31",
+    departementLibelle: "Haute Garonne",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/32-Gers.jpg",
+    departementCode: "32",
+    departementLibelle: "Gers",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/33-Gironde.jpg",
+    departementCode: "33",
+    departementLibelle: "Gironde",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/34-Herault.jpg",
+    departementCode: "34",
+    departementLibelle: "Hérault",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/35-Ille_et_Vilaine.jpg",
+    departementCode: "35",
+    departementLibelle: "Ille et Vilaine",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/36-Indre.jpg",
+    departementCode: "36",
+    departementLibelle: "Indre",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/37-Indre_et_Loire.jpg",
+    departementCode: "37",
+    departementLibelle: "Indre et Loire",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/38-Isere.jpg",
+    departementCode: "38",
+    departementLibelle: "Isère",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/39-Jura.jpg",
+    departementCode: "39",
+    departementLibelle: "Jura",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/40-Landes.jpg",
+    departementCode: "40",
+    departementLibelle: "Landes",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/41-Loir_et_Cher.jpg",
+    departementCode: "41",
+    departementLibelle: "Loir et Cher",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/42-Loire.jpg",
+    departementCode: "42",
+    departementLibelle: "Loire",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/43-Haute_Loire.jpg",
+    departementCode: "43",
+    departementLibelle: "Haute Loire",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/44-Loire_Atlantique.jpg",
+    departementCode: "44",
+    departementLibelle: "Loire Atlantique",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/45-Loiret.jpg",
+    departementCode: "45",
+    departementLibelle: "Loiret",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/46-Lot.jpg",
+    departementCode: "46",
+    departementLibelle: "Lot",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/47-Lot_et_Garonne.jpg",
+    departementCode: "47",
+    departementLibelle: "Lot et Garonne",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/48-Lozere.jpg",
+    departementCode: "48",
+    departementLibelle: "Lozère",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/49-Maine_et_Loire.jpg",
+    departementCode: "49",
+    departementLibelle: "Maine et Loire",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/50-Manche.jpg",
+    departementCode: "50",
+    departementLibelle: "Manche",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/51-Marne.jpg",
+    departementCode: "51",
+    departementLibelle: "Marne",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/52-Haute_Marne.jpg",
+    departementCode: "52",
+    departementLibelle: "Haute Marne",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/53-Mayenne.jpg",
+    departementCode: "53",
+    departementLibelle: "Mayenne",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/54-Meurthe_et_Moselle.jpg",
+    departementCode: "54",
+    departementLibelle: "Meurthe et Moselle",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/55-Meuse.jpg",
+    departementCode: "55",
+    departementLibelle: "Meuse",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/56-Morbihan.jpg",
+    departementCode: "56",
+    departementLibelle: "Morbihan",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/57-Moselle.jpg",
+    departementCode: "57",
+    departementLibelle: "Moselle",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/58-Nievre.jpg",
+    departementCode: "58",
+    departementLibelle: "Nièvre",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/59-Nord.jpg",
+    departementCode: "59",
+    departementLibelle: "Nord",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/60-Oise.jpg",
+    departementCode: "60",
+    departementLibelle: "Oise",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/61-Orne.jpg",
+    departementCode: "61",
+    departementLibelle: "Orne",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/62-Pas_de_Calais.jpg",
+    departementCode: "62",
+    departementLibelle: "Pas de Calais",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/63-Puy_de_Dome.jpg",
+    departementCode: "63",
+    departementLibelle: "Puy de Dôme",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/64-Pyrenees_Atlantiques.jpg",
+    departementCode: "64",
+    departementLibelle: "Pyrénées Atlantiques",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/65-Hautes_Pyrenees.jpg",
+    departementCode: "65",
+    departementLibelle: "Hautes Pyrénées",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/66-Pyrenees_Orientales.jpg",
+    departementCode: "66",
+    departementLibelle: "Pyrénées Orientales",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/67-Bas_Rhin.jpg",
+    departementCode: "67",
+    departementLibelle: "Bas Rhin",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/68-Haut_Rhin.jpg",
+    departementCode: "68",
+    departementLibelle: "Haut Rhin",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/69-Rhone.jpg",
+    departementCode: "69",
+    departementLibelle: "Rhône",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/70-Haute_Saone.jpg",
+    departementCode: "70",
+    departementLibelle: "Haute Saône",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/71-Saone_et_Loire.jpg",
+    departementCode: "71",
+    departementLibelle: "Saône et Loire",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/72-Sarthe.jpg",
+    departementCode: "72",
+    departementLibelle: "Sarthe",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/73-Savoie.jpg",
+    departementCode: "73",
+    departementLibelle: "Savoie",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/74-Haute_Savoie.jpg",
+    departementCode: "74",
+    departementLibelle: "Haute Savoie",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/75-Paris.jpg",
+    departementCode: "75",
+    departementLibelle: "Paris",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/76-Seine_Maritime.jpg",
+    departementCode: "76",
+    departementLibelle: "Seine Maritime",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/77-Seine_et_Marne.jpg",
+    departementCode: "77",
+    departementLibelle: "Seine et Marne",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/78-Yvelines.jpg",
+    departementCode: "78",
+    departementLibelle: "Yvelines",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/79-Deux_Sevres.jpg",
+    departementCode: "79",
+    departementLibelle: "Deux Sèvres",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/80-Somme.jpg",
+    departementCode: "80",
+    departementLibelle: "Somme",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/81-Tarn.jpg",
+    departementCode: "81",
+    departementLibelle: "Tarn",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/82-Tarn_et_Garonne.jpg",
+    departementCode: "82",
+    departementLibelle: "Tarn et Garonne",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/83-Var.jpg",
+    departementCode: "83",
+    departementLibelle: "Var",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/84-Vaucluse.jpg",
+    departementCode: "84",
+    departementLibelle: "Vaucluse",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/85-Vendee.jpg",
+    departementCode: "85",
+    departementLibelle: "Vendée",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/86-Vienne.jpg",
+    departementCode: "86",
+    departementLibelle: "Vienne",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/87-Haute_Vienne.jpg",
+    departementCode: "87",
+    departementLibelle: "Haute Vienne",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/88-Vosges.jpg",
+    departementCode: "88",
+    departementLibelle: "Vosges",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/89-Yonne.jpg",
+    departementCode: "89",
+    departementLibelle: "Yonne",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/90-Territoire_de_Belfort.jpg",
+    departementCode: "90",
+    departementLibelle: "Territoire de Belfort",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/91-Essonne.jpg",
+    departementCode: "91",
+    departementLibelle: "Essonne",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/92-Hauts_de_Seine.jpg",
+    departementCode: "92",
+    departementLibelle: "Hauts de Seine",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/93-Seine_Saint_Denis.jpg",
+    departementCode: "93",
+    departementLibelle: "Seine Saint Denis",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/94-Val_de_Marne.jpg",
+    departementCode: "94",
+    departementLibelle: "Val de Marne",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/95-Val_d_Oise.jpg",
+    departementCode: "95",
+    departementLibelle: "Val d'Oise",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/Seine.jpg",
+    departementCode: "75",
+    departementLibelle: "Seine",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/78-Yvelines.jpg",
+    departementCode: "78",
+    departementLibelle: "Seine et Oise",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/971-Guadeloupe.jpg",
+    departementCode: "971",
+    departementLibelle: "Guadeloupe",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/972-Martinique.jpg",
+    departementCode: "972",
+    departementLibelle: "Martinique",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/973-Guyane.jpg",
+    departementCode: "973",
+    departementLibelle: "Guyane",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/974-La_Reunion.jpg",
+    departementCode: "974",
+    departementLibelle: "La Réunion",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/975-Saint_Pierre_et_Miquelon.jpg",
+    departementCode: "975",
+    departementLibelle: "Saint Pierre et Miquelon",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/976-Mayotte.jpg",
+    departementCode: "976",
+    departementLibelle: "Mayotte",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/977-Saint_Barthelemy.jpg",
+    departementCode: "977",
+    departementLibelle: "Saint Barthélemy",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/vierge.jpg",
+    departementCode: "978",
+    departementLibelle: "Saint",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/986-Wallis_et_Futuna.jpg",
+    departementCode: "986",
+    departementLibelle: "Wallis et Futuna",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/vierge.jpg",
+    departementCode: "987",
+    departementLibelle: "Polynésie Française",
+  },
+  {
+    blasonURL: "https://armorialdefrance.fr/images/departements/vierge.jpg",
+    departementCode: "988",
+    departementLibelle: "Nouvelle Calédonie",
+  },
+  {
+    blasonURL:
+      "https://armorialdefrance.fr/images/departements/984-Terres_australes_et_antarctiques_francaises.jpg",
+    departementCode: "984",
+    departementLibelle: "Terres Australes et Antarctiques Françaises",
+  },
+];
